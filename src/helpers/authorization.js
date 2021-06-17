1537cf29c896ced98127242c95d60770704272f3
ehsan <ehsan@raychat.io>
2021-06-17 16:01:55 +0430

    code formatter

--- src/helpers/authorization.js
+++ src/helpers/authorization.js
@@ -5,74 +5,79 @@ import { AccessControl } from 'role-acl';
 const AC = new AccessControl();
 
 class Authorization {
-    constructor() {
-    }
+  constructor() {
+  }
 
-    verifyToken(data) {
-        return data;
-    }
+  verifyToken(data) {
+    return data;
+  }
 
-    verifyRole(address, userType) {
+  verifyRole(address, userType) {
 
-    }
+  }
 
-    defineRoles() {
+  defineRoles() {
 
-        AC.grant('deleted');
+    AC.grant('deleted');
 
-        AC.grant('user')
-            .execute('comment-create').on('POST')
-            .execute('comment-get').on('GET')
-            .execute('superadmin-option-find').on('GET')
-            .execute('superadmin-option-findOne').on('GET')
-            .execute('user-token-refresh').on('POST')
-            .execute('user-membership').on('GET')
-            .execute('user-id').on('GET')
-            .execute('user-profile-update').on('PUT')
-            .execute('user-remove').on('delete')
-            .execute('order-create').on('POST')
-            .execute('order-get').on('GET')
-            .execute('offer-get-one').on('GET');
+    AC.grant('user')
+      .execute('comment-create').on('POST')
+      .execute('comment-get').on('GET')
+      .execute('superadmin-option-find').on('GET')
+      .execute('superadmin-option-findOne').on('GET')
+      .execute('user-token-refresh').on('POST')
+      .execute('user-membership').on('GET')
+      .execute('user-id').on('GET')
+      .execute('user-profile-update').on('PUT')
+      .execute('user-remove').on('delete')
+      .execute('order-create').on('POST')
+      .execute('order-get').on('GET')
+      .execute('offer-get-one').on('GET')
+      .execute('order-cancel').on('PUT');
 
-        AC.grant('affiliate')
-            .extend('user')
-            .execute('comment-update').on('PUT')
-            .execute('wallet-get').on('GET')
-            .execute('wallet-create').on('POST')
-            .execute('wallet-update').on('PUT');
+    AC.grant('wholesale')
+      .extend('user');
 
-
-        AC.grant('vip')
-            .extend('affiliate')
-            .execute('product-create').on('POST')
-            .execute('product-update').on('PUT')
-            .execute('product-delete').on('DELETE')
-            .execute('superadmin-option-create').on('POST')
-            .execute('superadmin-option-update').on('PUT')
-            .execute('superadmin-option-delete').on('DELETE')
-            .execute('offer-create').on('POST')
-            .execute('offer-update').on('PUT')
-            .execute('offer-get').on('GET')
-            .execute('offer-get-one-dashboard').on('GET')
-            .execute('user-profile-update-telegram').on('PUT')
-            .execute('list-get').on('GET')
-            .execute('list-getOne').on('GET')
-            .execute('list-create').on('POST')
-            .execute('list-update').on('PUT')
-            .execute('list-delete').on('DELETE');
+    AC.grant('affiliate')
+      .extend('user')
+      .execute('comment-update').on('PUT')
+      .execute('wallet-get').on('GET')
+      .execute('wallet-create').on('POST')
+      .execute('wallet-update').on('PUT');
 
 
+    AC.grant('vip')
+      .extend('affiliate')
+      .execute('product-create').on('POST')
+      .execute('product-update').on('PUT')
+      .execute('product-delete').on('DELETE')
+      .execute('superadmin-option-create').on('POST')
+      .execute('superadmin-option-update').on('PUT')
+      .execute('superadmin-option-delete').on('DELETE')
+      .execute('offer-create').on('POST')
+      .execute('offer-update').on('PUT')
+      .execute('offer-get').on('GET')
+      .execute('offer-get-one-dashboard').on('GET')
+      .execute('user-profile-update-telegram').on('PUT')
+      .execute('list-get').on('GET')
+      .execute('list-getOne').on('GET')
+      .execute('list-create').on('POST')
+      .execute('list-update').on('PUT')
+      .execute('list-delete').on('DELETE');
 
-        AC.grant('superadmin')
-            .extend('vip')
-            .execute('comment-update').on('PUT')
-            .execute('comment-DELETE').on('DELETE')
-            .execute('category-create').on('POST')
-            .execute('category-update').on('PUT')
-            .execute('category-delete').on('DELETE');
 
-        return AC;
-    }
+
+    AC.grant('superadmin')
+      .extend('vip')
+      .execute('comment-update').on('PUT')
+      .execute('comment-DELETE').on('DELETE')
+      .execute('category-create').on('POST')
+      .execute('category-update').on('PUT')
+      .execute('category-delete').on('DELETE')
+      .execute('superadmin-profile-update').on('PUT');
+
+    return AC;
+  }
 }
 
 export default Authorization;
