30d3c982752e904fc739fff80eaae03c05bc3b30
ehsan <ehsan@raychat.io>
2021-06-19 20:50:30 +0430

    code formatter

--- src/models/user/user.js
+++ src/models/user/user.js
@@ -8,54 +8,58 @@ set('useFindAndModify', false);
 set('useCreateIndex', true);
 
 const schema = new Schema(
-    {
-        phone: { type: String, required: true, unique: true, index: true },
-        salt: { type: String, required: false },
-        password: { type: String, required: false },
-        status: { type: Number, required: false, default: 0 },
-        type: { type: Number, required: false, default: 0 },
-        email: { type: String, required: false },
-        username: { type: String, required: false, min: 8 },
-        name: { type: String, required: false },
-        family: { type: String, required: false },
-        avatar: { type: String, required: false, default: null },
-        nationalCode: { type: String, required: false },
-        offerCodes: { type: Array, required: false, default: [] },
-        secret: { type: String, required: false },
-        locations: [{
-            default: { type: Boolean, required: true, default: false },
-            lat: { type: String, required: true },
-            lng: { type: String, required: true },
-            originalAddress: { type: String, required: true },
-            state: { type: String, required: true },
-            country: { type: String, required: false },
-            city: { type: String, required: true },
-            village: { type: String, required: true },
-            zipCode: { type: String, required: true },
-            plaque: { type: String, required: true },
-            hasReceiver: { type: Boolean, required: true },
-            receiver: {
-                firstName: { type: String, required: false },
-                lastName: { type: String, required: false },
-                nationalId: { type: String, required: false },
-                phoneNumber: { type: String, required: false },
-            },
-        }],
-        telegram: {
-            id: { type: String, required: false },
-            first_name: { type: String, required: false },
-            username: { type: String, required: false },
-            type: { type: String, required: false },
-        },
-        Installment: {
-            check: { type: String, required: false },
-            turnover: { type: String, required: false },
-            nationalCard: { type: String, required: false },
+  {
+    phone: { type: String, required: true, unique: true, index: true },
+    salt: { type: String, required: false },
+    password: { type: String, required: false },
+    status: { type: Number, required: false, default: 0 },
+    type: { type: Number, required: false, default: 0 },
+    email: { type: String, required: false },
+    username: { type: String, required: false, min: 8 },
+    name: { type: String, required: false },
+    family: { type: String, required: false },
+    avatar: { type: String, required: false, default: null },
+    nationalCode: { type: String, required: false },
+    offerCodes: { type: Array, required: false, default: [] },
+    secret: { type: String, required: false },
+    locations: [{
+      default: { type: Boolean, required: true, default: false },
+      lat: { type: String, required: true },
+      lng: { type: String, required: true },
+      originalAddress: { type: String, required: true },
+      state: { type: String, required: true },
+      country: { type: String, required: false },
+      city: { type: String, required: true },
+      village: { type: String, required: true },
+      zipCode: { type: String, required: true },
+      plaque: { type: String, required: true },
+      hasReceiver: { type: Boolean, required: true },
+      receiver: {
+        firstName: { type: String, required: false },
+        lastName: { type: String, required: false },
+        nationalId: { type: String, required: false },
+        phoneNumber: { type: String, required: false },
+      },
+    }],
+    telegram: {
+      id: { type: String, required: false },
+      first_name: { type: String, required: false },
+      username: { type: String, required: false },
+      type: { type: String, required: false },
+    },
+    Installment: {
+      check: { type: String, required: false },
+      turnover: { type: String, required: false },
+      nationalCard: { type: String, required: false },
     },
     bio: {
       description: { type: String, required: false },
       files: { type: Array, required: false },
     }
+  },
+  {
+    minimize: false,
+    versionKey: false,
   },
 );
 
@@ -67,9 +71,9 @@ const modelSchema = model('users', schema);
 
 // Index fields
 modelSchema.ensureIndexes((err) => {
-    if (err) {
-        console.error('user model error :', err.message);
-    }
+  if (err) {
+    console.error('user model error :', err.message);
+  }
 });
 
 export default modelSchema;
