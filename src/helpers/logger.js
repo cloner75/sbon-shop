b69b3d9f5c9a8b439bf23db348bd000dd24a4308
ehsan <ehsan@raychat.io>
2021-06-24 22:13:57 +0430

    update pino config

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -2,8 +2,8 @@
 import pino from 'pino';
 
 export default pino(pino.destination({
-  level: process.env.LOG_LEVEL,
+  level: process.env.LOG_LEVEL || 'info',
   name: 'spon.user',
-  dest: 'log.log', // omit for stdout
+  // dest: 'log.log', // omit for stdout
   sync: false // Asynchronous logging
 }));
