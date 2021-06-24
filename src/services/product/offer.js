468df8c509f541679d846d2e810a644e29da3b45
ehsan <ehsan@raychat.io>
2021-06-24 15:52:22 +0430

    static and code formatter  offerService

--- src/services/product/offer.js
+++ src/services/product/offer.js
@@ -12,19 +12,21 @@ import offerModel from "./../../models/product/offer";
  * var Ctrl = new Controller();
  */
 export default class OfferService {
-    /**
-     * @description :: Get One Document
+  /**
+   * @description :: Get One Document
+   * @param {string} code 
      * @param {string} code 
-     */
-    async findOne(code) {
-        const result = await offerModel.findOne({ code });
-        if (!result || !result.status) {
-            return { status: 404 };
-        } else if (moment().locale('fa').format('YYYY-MM-DD') > moment(result.expirationTime).format('YYYY-MM-DD')) {
-            await offerModel.updateOne({ code: req.params.code }, { status: 0 });
-            return { status: 404 };
-        } else {
-            return { status: 200, result };
-        }
+   * @param {string} code 
+   */
+  static async findOne(code) {
+    const result = await offerModel.findOne({ code });
+    if (!result || !result.status) {
+      return { status: 404 };
+    } else if (moment().locale('fa').format('YYYY-MM-DD') > moment(result.expirationTime).format('YYYY-MM-DD')) {
+      await offerModel.updateOne({ code: req.params.code }, { status: 0 });
+      return { status: 404 };
+    } else {
+      return { status: 200, result };
     }
+  }
 }
