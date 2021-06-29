a3f10b17f8fa82542b27ca0e52889def9bf172d6
ehsan <ehsan@raychat.io>
2021-06-29 23:59:53 +0430

    add logger class

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,9 +1,51 @@
 // package 
 import pino from 'pino';
 
-export default pino(pino.destination({
+const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
-  name: 'spon.user',
-  // dest: 'log.log', // omit for stdout
+}, pino.destination({
+  name: process.env.LOG_NAME || 'spon.user',
+  dest: 'log.log', // omit for stdout
   sync: false // Asynchronous logging
 }));
+
+class Logger {
+  constructor(serviceName) {
+    this.serviceName = serviceName;
+  }
+
+  /**
+   * @description :: set info log
+   * @param {string} method 
+   * @param {number} time 
+   * @param {string} message 
+   */
+  info(method, time, message = '200') {
+    logger.info({
+      service: this.serviceName,
+      isSuccess: true,
+      executionTime: Date.now() - time,
+      method,
+      message,
+    });
+  }
+
+  /**
+   * @description :: set error log
+   * @param {string} method 
+   * @param {number} time 
+   * @param {object} error 
+   */
+  error(method, time, error = {}) {
+    Logger.error({
+      service: this.serviceName,
+      isSuccess: false,
+      message: error.message,
+      stack: error.stack,
+      method,
+      executionTime: Date.now() - time
+    });
+  }
+}
+
+export default Logger;
