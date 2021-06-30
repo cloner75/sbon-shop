9fd5c873cdbda0eff8ad8be8352ae75e64d239c1
ehsan <ehsan@raychat.io>
2021-06-30 23:17:27 +0430

    code formatter

--- src/controllers/product/offer.js
+++ src/controllers/product/offer.js
@@ -4,8 +4,7 @@ import userModel from './../../models/user/user';
 
 // Helper
 import MongoHelper from "./../../helpers/mongo";
-import Response from "./../../helpers/response";
-import Logger from "./../../helpers/logger";
+import ResponseGenerator from "./../../helpers/response";
 
 // Services
 import OfferService from './../../services/product/offer';
@@ -13,6 +12,15 @@ import OfferService from './../../services/product/offer';
 
 // Consts
 const OFFER = 'offer';
+const Response = new ResponseGenerator('comment-service');
+const METHODS = {
+  CREATE: 'create',
+  FIND: 'find',
+  FIND_PUBLIC: 'find-public',
+  FIND_ONE: 'find-one',
+  REMOVE: 'remove',
+  UPDATE: 'update'
+};
 
 /**
  * @description :: The Controller service
@@ -22,180 +30,180 @@ const OFFER = 'offer';
  * var Ctrl = new Controller();
  */
 export default class Offer extends OfferService {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    /**
-     * @description :: Get Documents
-     * @param {request} req 
-     * @param {Reply} reply 
-     */
-    async find(req, reply) {
-        const start = Date.now();
-        try {
-            const { where, options } = MongoHelper.initialMongoQuery(req.query, OFFER);
-            const result = await offerModel.paginate(where, options);
-            Logger.info({
-                controller: 'Offer',
-                api: 'find',
-                isSuccess: true,
-                ip: req.clientIp,
-                message: '200',
-                time: Date.now() - start
-            });
+  /**
+   * @description :: Get Documents
+   * @param {request} req 
+   * @param {Reply} reply 
+   */
+  async find(req, reply) {
+    const start = Date.now();
+    try {
+      const { where, options } = MongoHelper.initialMongoQuery(req.query, OFFER);
+      const result = await offerModel.paginate(where, options);
+      Logger.info({
+        controller: 'Offer',
+        api: 'find',
+        isSuccess: true,
+        ip: req.clientIp,
+        message: '200',
+        time: Date.now() - start
+      });
+      return reply.send(Response.generator(200, result));
+    } catch (err) {
+      Logger.error({
+        controller: 'Offer',
+        api: 'find',
+        isSuccess: false,
+        ip: req.clientIp,
+        message: err.message,
+        time: Date.now() - start
+      });
+      return reply.status(500).send(Response.generator(500, err.message));
+    }
+  }
+
+  /**
+   * @description :: Get One Document
+   * @param {request} req 
+   * @param {Reply} reply 
+   */
+  async findOne(req, reply) {
+    const start = Date.now();
+    try {
+      const { status, result } = await super.findOne(req.params.code);
+      Logger.info({
+        controller: 'Offer',
+        api: 'findOne',
+        isSuccess: true,
+        ip: req.clientIp,
+        message: '200',
+        time: Date.now() - start
+      });
+      if (status === 200) {
+        const getUser = await userModel.findOne({ _id: req.user._id });
+        if (getUser) {
+          if (!getUser.offerCodes.includes(req.params.code)) {
             return reply.send(Response.generator(200, result));
-        } catch (err) {
-            Logger.error({
-                controller: 'Offer',
-                api: 'find',
-                isSuccess: false,
-                ip: req.clientIp,
-                message: err.message,
-                time: Date.now() - start
-            });
-            return reply.status(500).send(Response.generator(500, err.message));
+          }
         }
+      }
+      return reply.status(404).send(Response.generator(404));
+    } catch (err) {
+      Logger.error({
+        controller: 'Offer',
+        api: 'findOne',
+        isSuccess: false,
+        ip: req.clientIp,
+        message: err.message,
+        time: Date.now() - start
+      });
+      return reply.status(500).send(Response.generator(500, err.message));
     }
+  }
 
-    /**
-     * @description :: Get One Document
-     * @param {request} req 
-     * @param {Reply} reply 
-     */
-    async findOne(req, reply) {
-        const start = Date.now();
-        try {
-            const { status, result } = await super.findOne(req.params.code);
-            Logger.info({
-                controller: 'Offer',
-                api: 'findOne',
-                isSuccess: true,
-                ip: req.clientIp,
-                message: '200',
-                time: Date.now() - start
-            });
-            if (status === 200) {
-                const getUser = await userModel.findOne({ _id: req.user._id });
-                if (getUser) {
-                    if (!getUser.offerCodes.includes(req.params.code)) {
-                        return reply.send(Response.generator(200, result));
-                    }
-                }
-            }
-            return reply.status(404).send(Response.generator(404));
-        } catch (err) {
-            Logger.error({
-                controller: 'Offer',
-                api: 'findOne',
-                isSuccess: false,
-                ip: req.clientIp,
-                message: err.message,
-                time: Date.now() - start
-            });
-            return reply.status(500).send(Response.generator(500, err.message));
-        }
+  /**
+   * @description :: Find One For DashBoard
+   * @param {request} req 
+   * @param {Reply} reply 
+   */
+  async findOneDashboard(req, reply) {
+    const start = Date.now();
+    try {
+      const { where, options } = MongoHelper.initialMongoQuery(req.query, OFFER);
+      const result = await offerModel.paginate({ ...where, _id: req.params.id }, options);
+      Logger.info({
+        controller: 'Offer',
+        api: 'findOneDashabord',
+        isSuccess: true,
+        ip: req.clientIp,
+        message: '200',
+        time: Date.now() - start
+      });
+      return reply.send(Response.generator(200, result));
+    } catch (err) {
+      Logger.error({
+        controller: 'Offer',
+        api: 'findOneDashabord',
+        isSuccess: false,
+        ip: req.clientIp,
+        message: err.message,
+        time: Date.now() - start
+      });
+      return reply.status(500).send(Response.generator(500, err.message));
     }
+  }
 
-    /**
-     * @description :: Find One For DashBoard
-     * @param {request} req 
-     * @param {Reply} reply 
-     */
-    async findOneDashboard(req, reply) {
-        const start = Date.now();
-        try {
-            const { where, options } = MongoHelper.initialMongoQuery(req.query, OFFER);
-            const result = await offerModel.paginate({ ...where, _id: req.params.id }, options);
-            Logger.info({
-                controller: 'Offer',
-                api: 'findOneDashabord',
-                isSuccess: true,
-                ip: req.clientIp,
-                message: '200',
-                time: Date.now() - start
-            });
-            return reply.send(Response.generator(200, result));
-        } catch (err) {
-            Logger.error({
-                controller: 'Offer',
-                api: 'findOneDashabord',
-                isSuccess: false,
-                ip: req.clientIp,
-                message: err.message,
-                time: Date.now() - start
-            });
-            return reply.status(500).send(Response.generator(500, err.message));
-        }
+  /**
+   * @description :: Create Document
+   * @param {request} req 
+   * @param {Reply} reply 
+   */
+  async create(req, reply) {
+    const start = Date.now();
+    try {
+      const result = await offerModel.create({ ...req.body, ownerId: req.user._id });
+      Logger.info({
+        controller: 'Offer',
+        api: 'create',
+        isSuccess: true,
+        ip: req.clientIp,
+        message: '200',
+        time: Date.now() - start
+      });
+      return reply.status(200).send(Response.generator(200, result));
+    } catch (err) {
+      Logger.error({
+        controller: 'Offer',
+        api: 'create',
+        isSuccess: true,
+        ip: req.clientIp,
+        message: '200',
+        time: Date.now() - start
+      });
+      return reply.status(500).send(err);
     }
+  }
 
-    /**
-     * @description :: Create Document
-     * @param {request} req 
-     * @param {Reply} reply 
-     */
-    async create(req, reply) {
-        const start = Date.now();
-        try {
-            const result = await offerModel.create({ ...req.body, ownerId: req.user._id });
-            Logger.info({
-                controller: 'Offer',
-                api: 'create',
-                isSuccess: true,
-                ip: req.clientIp,
-                message: '200',
-                time: Date.now() - start
-            });
-            return reply.status(200).send(Response.generator(200, result));
-        } catch (err) {
-            Logger.error({
-                controller: 'Offer',
-                api: 'create',
-                isSuccess: true,
-                ip: req.clientIp,
-                message: '200',
-                time: Date.now() - start
-            });
-            return reply.status(500).send(err);
-        }
+  /**
+   * @description :: update Documents
+   * @param {request} req 
+   * @param {Reply} reply 
+   */
+  async update(req, reply) {
+    const start = Date.now();
+    try {
+      const result = await offerModel.findOneAndUpdate({
+        _id: req.params.id,
+        // minishop: req.user.minishop
+      },
+        { ...req.body, ownerId: req.user._id },
+        { new: true }
+      );
+      Logger.info({
+        controller: 'Offer',
+        api: 'update',
+        isSuccess: true,
+        ip: req.clientIp,
+        message: '200',
+        time: Date.now() - start
+      });
+      return result ?
+        reply.status(200).send(Response.generator(200, result)) :
+        reply.status(404).send(Response.generator(404));
+    } catch (err) {
+      Logger.error({
+        controller: 'Offer',
+        api: 'update',
+        isSuccess: true,
+        ip: req.clientIp,
+        message: '200',
+        time: Date.now() - start
+      });
+      return reply.status(500).send(err);
     }
-
-    /**
-     * @description :: update Documents
-     * @param {request} req 
-     * @param {Reply} reply 
-     */
-    async update(req, reply) {
-        const start = Date.now();
-        try {
-            const result = await offerModel.findOneAndUpdate({
-                _id: req.params.id,
-                // minishop: req.user.minishop
-            },
-                { ...req.body, ownerId: req.user._id },
-                { new: true }
-            );
-            Logger.info({
-                controller: 'Offer',
-                api: 'update',
-                isSuccess: true,
-                ip: req.clientIp,
-                message: '200',
-                time: Date.now() - start
-            });
-            return result ?
-                reply.status(200).send(Response.generator(200, result)) :
-                reply.status(404).send(Response.generator(404));
-        } catch (err) {
-            Logger.error({
-                controller: 'Offer',
-                api: 'update',
-                isSuccess: true,
-                ip: req.clientIp,
-                message: '200',
-                time: Date.now() - start
-            });
-            return reply.status(500).send(err);
-        }
-    }
+  }
 }
