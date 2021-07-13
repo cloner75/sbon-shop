5ed52a67a5ed16c5d0cacf4f0237a40fb06cc4de
ehsan <ehsan@raychat.io>
2021-07-14 00:48:29 +0430

    export models in index file

---
+++ src/models/index.js
@@ -0,0 +1,25 @@
+import user from './user/user';
+import list from './user/list';
+import order from './user/order';
+import wallet from './user/wallet';
+import brand from './product/brand';
+import category from './product/category';
+import comment from './product/comment';
+import installment from './product/installment';
+import offer from './product/offer';
+import product from './product/product';
+import opiton from './option/index';
+
+export default {
+  user,
+  list,
+  order,
+  wallet,
+  brand,
+  category,
+  comment,
+  installment,
+  offer,
+  product,
+  opiton,
+};
