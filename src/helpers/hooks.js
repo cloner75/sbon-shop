24c489a6f883b8f36d3d55e4451920345b4019e3
ehsan@raychat.io <ehsan@raychat.io>
2021-08-18 00:14:50 +0430

    refactor authorzation cookie , functional,refreshToken

--- src/helpers/hooks.js
+++ src/helpers/hooks.js
@@ -1,9 +1,12 @@
+// Models
+import UserModel from './../models/user/user';
+
 // Helpers
 import logger from './logger';
+import cookieConfig from './../configs/cookie';
 import JWT from './jsonwebtoken';
 import Auth from './authorization';
 import ResponseGenerator from './response';
-
 // Config
 import authConfig from './../configs/auth';
 
@@ -12,6 +15,9 @@ const Response = new ResponseGenerator('hooks-service');
 const AC = new Auth();
 const jwt = new JWT();
 const role = AC.defineRoles();
+const METHODS = {
+  refreshToken: 'refresh_token'
+};
 const userType = {
   0: 'user',
   1: 'affiliate',
@@ -20,6 +26,75 @@ const userType = {
   4: 'deleted',
   5: 'wholesale'
 };
+
+/**
+ * @description :: check authorization utils
+ * @param {request} req 
+ * @param {reply} reply 
+ * @returns 
+ */
+async function checkAuthorization(req, reply) {
+  if (authConfig[req.routerPath] && authConfig[req.routerPath].isAuth) {
+    if (req.headers.authorization || req.cookies.access_token) {
+      req.user = jwt.verify(req.cookies.access_token || req.headers.authorization);
+      const permission = await role
+        .can(userType[req.user.type])
+        .execute(authConfig[req.routerPath].name)
+        .on(authConfig[req.routerPath].method);
+      if (!permission.granted) {
+        return reply.status(403).send(
+          Response.generator(
+            403,
+            {},
+            'authorization',
+            req.executionTime
+          ));
+      }
+    } else {
+      return reply.status(401).send(
+        Response.generator(
+          401,
+          {},
+          'authorization',
+          req.executionTime
+        ));
+    }
+  }
+}
+
+/**
+ * @description set new token for client
+ * @param {request} request 
+ * @param {reply} reply 
+ * @param {string} token 
+ */
+async function refreshToken(req, reply, token) {
+  const decode = jwt.decode(token);
+  const getUser = await UserModel.findById(decode._id);
+  if (!getUser) {
+    return reply.status(404).send(
+      Response.generator(404, {}, METHODS.refreshToken, req.executionTime)
+    );
+  }
+  const { password, salt, phone, ...result } = getUser.toObject();
+  token = await jwt.generate(result);
+  reply
+    .status(403)
+    .setCookie(
+      'access_token',
+      token,
+      cookieConfig
+    )
+    .send(
+      Response.generator(
+        403,
+        { message: 'jwt expired' },
+        'authorization',
+        req.executionTime
+      )
+    );
+}
+
 
 /**
  * @description :: Class For Hooks Service
@@ -33,41 +108,20 @@ class Hooks {
   static async authorization(req, reply) {
     req.executionTime = Date.now();
     try {
-      if (authConfig[req.routerPath] && authConfig[req.routerPath].isAuth) {
-        if (req.headers.authorization) {
-          req.user = jwt.verify(req.headers.authorization);
-          const permission = await role
-            .can(userType[req.user.type])
-            .execute(authConfig[req.routerPath].name)
-            .on(authConfig[req.routerPath].method);
-          if (!permission.granted) {
-            return reply.status(403).send(
-              Response.generator(
-                403,
-                {},
-                'authorization',
-                req.executionTime
-              ));
-          }
-        } else {
-          return reply.status(401).send(
-            Response.generator(
-              401,
-              {},
-              'authorization',
-              req.executionTime
-            ));
-        }
-      }
+      await checkAuthorization(req, reply);
     } catch (err) {
-      return reply.status(403).send(
-        Response.generator(
-          403,
-          { message: err.message },
-          'authorization',
-          req.executionTime
-        )
-      );
+      if (/jwt expired/.test(err.message)) {
+        await refreshToken(req, reply, req.cookies.access_token || req.headers.authorization);
+      } else {
+        return reply.status(403).send(
+          Response.generator(
+            403,
+            { message: err.message },
+            'authorization',
+            req.executionTime
+          )
+        );
+      }
     }
   };
 }
