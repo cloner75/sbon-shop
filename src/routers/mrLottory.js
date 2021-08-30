2a2dd9b2105a8d76050f7777ea43173850715c54
cloner75 <ehsan.younesi.1375@gmail.com>
2021-08-31 00:11:22 +0430

    inti  mrLottory router

---
+++ src/routers/mrLottory.js
@@ -0,0 +1,24 @@
+// Controllers
+import MrLottoryController from '../controllers/mrlottory';
+
+// Controller
+const MrLottory = new MrLottoryController();
+
+// Midds
+import Midd from './../middlewares/mrLottory';
+
+// Hookes
+import Hooks from './../helpers/hooks';
+
+// Routes
+export default (fastify, _opts, done) => {
+  // Hooks
+  fastify.addHook('onRequest', Hooks.authorization);
+
+  // User Routes
+  fastify.post('/create', Midd.create, MrLottory.createUser);
+  // fastify.get('/get', Midd.find, Option.find);
+  done();
+};
+
+
