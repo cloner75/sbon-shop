03c91d2ac9f4b100a5886091fd430b4e0fb0614d
cloner75 <ehsan.younesi.1375@gmail.com>
2021-08-31 00:11:04 +0430

    inti  mrLottory model

---
+++ src/models/mrlottory/user.js
@@ -0,0 +1,59 @@
+// Packages
+import { set, model, Schema } from 'mongoose';
+import paginate from 'mongoose-paginate';
+import timestamp from 'mongoose-timestamp';
+
+// Setting mongoose
+set('useFindAndModify', false);
+set('useCreateIndex', true);
+
+const schema = new Schema(
+  {
+    name: { type: String, required: true },
+    family: { type: String, required: true },
+    lastCertificate: { type: String, required: true },
+    gender: { type: String, required: true },
+    birthday: { type: String, required: true },
+    placeOfBirthday: { type: String, required: true },
+    picture: { type: String, required: false },
+    maritalStatus: { type: String, required: false },
+    countChild: { type: String, required: false },
+    childs: [{
+      name: { type: String, required: false },
+      family: { type: String, required: false },
+      lastCertificate: { type: String, required: false },
+      gender: { type: String, required: false },
+      birthday: { type: String, required: false },
+      placeOfBirthday: { type: String, required: false },
+      picture: { type: String, required: false },
+      maritalStatus: { type: String, required: false },
+    }],
+    passport: {
+      number: { type: String, rqeuired: false },
+      expired: { type: String, rqeuired: false },
+    },
+    status: { type: Number, required: false, default: 0 },// 0 => not Pay , 1 => Payeid
+    userId: { type: Schema.Types.ObjectId, required: false },
+    orderId: { type: String, rqeuired: false },
+    token: { type: String, rqeuired: false },
+  },
+  {
+    minimize: false,
+    versionKey: false,
+  },
+);
+
+// Add plugins
+schema.plugin(paginate);
+schema.plugin(timestamp);
+
+const modelSchema = model('lottory-users', schema);
+
+// Index fields
+modelSchema.ensureIndexes((err) => {
+  if (err) {
+    console.error('lottory-user model error :', err.message);
+  }
+});
+
+export default modelSchema;
