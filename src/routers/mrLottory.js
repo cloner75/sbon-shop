a494493a3d93b5bbc850263e15532690fbce1fe4
cloner75 <ehsan.younesi.1375@gmail.com>
2021-08-31 23:45:24 +0430

    add verify route

--- src/routers/mrLottory.js
+++ src/routers/mrLottory.js
@@ -17,6 +17,7 @@ export default (fastify, _opts, done) => {
 
   // User Routes
   fastify.post('/create', Midd.create, MrLottory.createUser);
+  fastify.post('/verify', Midd.verify, MrLottory.verify);
   // fastify.get('/get', Midd.find, Option.find);
   done();
 };
